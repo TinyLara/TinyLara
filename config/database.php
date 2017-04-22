d336a6420fde367b19cb18fabdcb99488aa603ba
sady <xxzhanglol@gmail.com>
2017-04-22 16:26:24 +0800

    修改本地数据库配置

--- config/database.php
+++ config/database.php
@@ -3,9 +3,9 @@
 return [
   'driver'    => 'mysql',
   'host'      => '127.0.0.1',
-  'database'  => 'tinylara',
-  'username'  => 'tinylara',
-  'password'  => 'password',
+  'database'  => 'mydb',
+  'username'  => 'root',
+  'password'  => 'LLawliet',
   'charset'   => 'utf8',
   'collation' => 'utf8_general_ci',
   'prefix'    => ''
